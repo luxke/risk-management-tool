7d1e585e8af7694d25260812c7b9c564e3fac574
Felix <kipkemboifelix576@gmail.com>
2026-07-04 22:44:21 +0300

    created a button add a new risk

--- app.py
+++ app.py
@@ -1,22 +1,71 @@
-from flask import Flask, render_template 
-from db import get_db_connection 
+from flask import Flask, render_template
+from db import get_db_connection
 
 app = Flask(__name__)
+
 @app.route('/')
 def dashboard():
-    conn = get_db_connection()
-    cursor = conn.cursor()
 
-    cursor.execute("SELECT COUNT(*) FROM risks")
-    total_risks = cursor.fetchone()[0]
+    conn = get_db_connection()
+    cursor = conn.cursor(dictionary=True)
+
+    # Total risks
+    cursor.execute("SELECT COUNT(*) AS total FROM risks")
+    total_risks = cursor.fetchone()['total']
+
+    # Get all risks
+    cursor.execute("""
+        SELECT
+            Risk_id,
+            Tittle,
+            Probability,
+            Impact,
+            Score,
+            status
+        FROM risks
+    """)
+
+    risks = cursor.fetchall()
+
     conn.close()
 
-    return render_template("dashboard.html", total_risks=total_risks)
+    return render_template(
+        "dashboard.html",
+        total_risks=total_risks,
+        risks=risks
+    )
+
+
+from flask import request, redirect
+
+@app.route('/add-risk', methods=['GET', 'POST'])
+def add_risk():
+
+    if request.method == 'POST':
+
+        Tittle = request.form['Tittle']
+        description = request.form['description']
+        probability = request.form['probability']
+        impact = request.form['impact']
+
+        score = int(probability) * int(impact)
+
+        conn = get_db_connection()
+        cursor = conn.cursor()
+
+        cursor.execute("""
+            INSERT INTO risks
+            (Tittle, Description, Probability, Impact, Score)
+            VALUES (%s,%s,%s,%s,%s)
+        """, (Tittle, description, probability, impact, score))
+
+        conn.commit()
+        conn.close()
+
+        return redirect('/')
+
+    return render_template("add_risk.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-
-
-
     
